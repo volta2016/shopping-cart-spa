3a762b2766cedc780b0ac5b474a40c1960f1ee59
Volta <giovoltadesign@gmail.com>
2021-01-03 00:57:51 -0300

    template products

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const openCart = document.querySelector('.cart__icon')
-const closeCart = document.querySelector('.close__cart')
-const productDOM = document.querySelector('.product__center')
-const cartDOM = document.querySelector('.cart_center')
+const openCart = document.querySelector('.cart__icon');
+const closeCart = document.querySelector('.close__cart');
+const productDOM = document.querySelector('.product__center');
+const cartDOM = document.querySelector('.cart_center');
 
 let cart = [];
 
@@ -10,7 +10,59 @@ let buttonDOM = []
 
 // UI
 class UI {
+  displayProducts(obj) {
+    console.log(obj)
+    let results = '';
+    obj.forEach(({id, title, price, image }) => {
+      results += `
+      <div class="product">
+      <div class="image__container">
+        <img src="${image}" alt="" />
+      </div>
+      <div class="product__footer">
+        <h1>${title}</h1>
+        <div class="rating">
+          <span>
+            <svg>
+              <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+            </svg>
+          </span>
+          <span>
+            <svg>
+              <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+            </svg>
+          </span>
+          <span>
+            <svg>
+              <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+            </svg>
+          </span>
+          <span>
+            <svg>
+              <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+            </svg>
+          </span>
+          <span>
+            <svg>
+              <use xlink:href="./images/sprite.svg#icon-star-empty"></use>
+            </svg>
+          </span>
+        </div>
+        <div class="bottom">
+          <div class="btn__group">
+            <a href="#" class="btn addToCart" data-id=${id}>Add to Cart</a>
+            <a href="#" class="btn view">View</a>
+          </div>
+          <div class="price">$${price}</div>
+        </div>
+      </div>
+    </div>
+      `
+    });
 
+    productDOM.innerHTML = results;
+
+  }
 }
 // Storage
 class Storage {
@@ -22,10 +74,12 @@ class Products {
   async getProduct(){
     try {
       const results = await fetch('products.json')
-      const data = await results.json()
-      console.log(data)
+      const data = await results.json();
+      const products = data.items;
+      // console.log(products);
+      return products;
     } catch(err) {
-      console.log(err)
+      console.log(err);
     }
   }
 }
@@ -34,5 +88,5 @@ document.addEventListener('DOMContentLoaded', async() => {
   const ui = new UI();
   const products = new Products();
   const productsOject = await products.getProduct();
-  
+  ui.displayProducts(productsOject)
 });
